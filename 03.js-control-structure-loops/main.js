2d6d52cf2ec625b5dae9ae9cd8c1b52fbfc341cf
nazrinamiraslanli <nazrinamiraslanli38@gmail.com>
2023-11-01 02:32:55 -0700

    task

---
+++ 03.js-control-structure-loops/main.js
@@ -0,0 +1,58 @@
+// // Task1
+// for (let index = 0; index < 100; index++) {
+//   if (index % 2 === 0) {
+//     console.log(`${index}`);
+//   }
+// }
+// // Task2
+// for (let index = 10; index < 100; index++) {
+//   if (index % 10 === 7) {
+//     console.log(`${index}`);
+//   }
+// }
+// // Task3
+// for (let index = 10; index < 100; index++) {
+//   if (index % 11 === 0) {
+//     console.log(`${index}`);
+//   }
+// }
+// // Task4
+// let str = "Nazrin";
+// let strLength = str.length;
+// for (i = 0; i < strLength; i++) {
+//   console.log(str[i]);
+// }
+// //  Task5
+// let a = 30;
+// for (let index = 0; index <= a; index++) {
+//   if (a % index === 0) console.log(index);
+// }
+// // Task6
+// for(var i = 15; i <= 30; i++){
+//     if(i%3==0 && i%5==0){
+//         console.log(`FizzBuzz`);
+//     }else if(i %3==0){
+//         console.log(`Fizz`);
+//     }else if(i%5==0){
+//         console.log(`Buzz`);
+//     }
+// }
+
+// // Task7
+// for(let i = 0; i < 50; i++){
+//     if (i >= 0 && i <= 15) continue
+//     else {
+//         console.log(i);
+//     }
+// }
+// // Task8
+// for(let i = 0; i < 15; i++){
+//     console.log(i);
+// }
+// // Task9
+// const n=7;
+// let factorial = 1;
+// for(let i =1; i<= n; i++){
+//     factorial *=i;
+// }
+// console.log(`${factorial}`);
