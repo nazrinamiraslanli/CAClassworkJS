4a0ec7bc2edc4ce07a10bdac70a6509440484e06
nazrinamiraslanli <nazrinamiraslanli38@gmail.com>
2023-11-03 02:55:39 -0700

    task

---
+++ 04.js-array&object/main.js
@@ -0,0 +1,30 @@
+// // Task1
+// let numbers = [12, 45, 6, 78, 34, 99, 23]
+// let max = numbers[0];
+// let min = numbers[0];
+
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > max) {
+//     max = numbers[i];
+//   }else if  (numbers[i] < min) {
+//     min = numbers[i];}
+//   }
+// console.log("MAX :", max , "MIN :" , min)
+// // Task2
+let numbers = [12, 45, 6, 78, 34, 99, 23];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+let avarege = sum / numbers.length;
+console.log("SUM", sum);
+// // Task3
+// let elements = [1, 2, 3, 4, 5];
+// let temp = elements[2];
+// elements[2] = elements[3];
+// elements[3] = temp;
+// console.log(elements)
+// Task4
+// let numbers = [12, -5, 6, -3, 34, -1, 23];
+// for(let i =0; i>0; i++){
+
