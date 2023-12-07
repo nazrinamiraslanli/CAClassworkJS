b6ddb4db9b62ed206b171f894421040c7a491892
[Nazrin Amiraslanli] <[nazrinamiraslanli38@gmail.com]>
2023-12-07 13:25:30 +0400

    task

---
+++ 26.js-blog-practics/home.js
@@ -0,0 +1,23 @@
+let cards = document.querySelector(".cards");
+const BASE_URL = "http://localhost:8080";
+async function getAllData() {
+  const res = await axios(`${BASE_URL}/blogs`);
+  
+  drawCards(res.data);
+}
+function drawCards(arr) {
+  arr.forEach((element) => {
+    cards.innerHTML += `
+      <div class="card">
+            <h3>${element.title}</h3>
+            <span>${element.body.slice(1, 3)}<a href="#">Read More</a></span>
+            <p>${element.author}</p>
+            <div class="but">
+              <button class="delBtn" onclick=delete>Delete</button>
+              <button class="editBtn" onclick=edit><a href="blog.html?">Edit</a></button>
+            </div>
+          
+        </div>
+        `;
+  });
+}
