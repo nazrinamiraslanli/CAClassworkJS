f4092c979329bb31ade426c36001bb9ff50daf29
nazrinamiraslanli <nazrinamiraslanli38@gmail.com>
2023-12-07 21:28:34 +0400

    task

--- 26.js-blog-practics/home.js
+++ 26.js-blog-practics/home.js
@@ -1,5 +1,5 @@
 let cards = document.querySelector(".cards");
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "  http://localhost:3000";
 async function getAllData() {
   const res = await axios(`${BASE_URL}/blogs`);
   
@@ -10,7 +10,7 @@ function drawCards(arr) {
     cards.innerHTML += `
       <div class="card">
             <h3>${element.title}</h3>
-            <span>${element.body.slice(1, 3)}<a href="#">Read More</a></span>
+            <span>${element.body.slice(1, 10)}<a href="#">Read More</a></span>
             <p>${element.author}</p>
             <div class="but">
               <button class="delBtn" onclick=delete>Delete</button>
