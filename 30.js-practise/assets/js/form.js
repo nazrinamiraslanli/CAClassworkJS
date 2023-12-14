465e9560dd5ccecdbf05dbb5e63df02a03e3248d
Nazrin Amiraslanli <nazrinamiraslanli38@gmail.com>
2023-12-14 10:51:58 +0400

    task

--- 30.js-practise/assets/js/form.js
+++ 30.js-practise/assets/js/form.js
@@ -5,9 +5,10 @@ const id = new URLSearchParams(window.location.search).get("id");
 async function fillForm() {
   const res = await axios(`${BASE_URL}/users/${id}`);
   console.log(res.data);
-  allInputs[0].value = res.data.firstName;
-  allInputs[1].value = res.data.lastName;
+  allInputs[0].value = res.data.name;
+  allInputs[1].value = res.data.surname;
   allInputs[2].value = res.data.email;
+  allInputs[3].value=res.data.userphoto
 }
 if (id) {
   fillForm();
@@ -17,9 +18,11 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   let user = {
-    firstName: allInputs[0].value,
-    lastName: allInputs[0].value,
-    Email: allInputs[0].value,
+    name: allInputs[0].value,
+    surname : allInputs[1].value,
+    email: allInputs[2].value,
+    userphoto: `./assets/img/${allInputs[3].value.split("\\")[2]}`,
+    date: date.toLocaleString(),
   };
 
   if (!id) {
